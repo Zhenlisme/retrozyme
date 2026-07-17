a49a21623e93761fbc71f16ed4c5676c210916bf
Nicolas Pollet <npollet@users.noreply.github.com>
2026-07-17 17:30:08 +0200

    Add files via upload
    Using here library to improve portability

--- scripts/Rtz_353_331_Ax.R
+++ scripts/Rtz_353_331_Ax.R
@@ -1,12 +1,14 @@
-
+library(here)
 library(ggplot2)
-rnafold_value_Amx=read.csv2('~/Toutatis_backup/Retrozyme/Retrozymes_detection/Ambystoma_mexicanum/Rtz353_331.rnafold',
+rnafold_value_Amx=read.csv2(here("retrozyme_data","Retrozymes_detection","Ambystoma_mexicanum","Rtz353_331.rnafold"),
                             stringsAsFactors = F, header = F)
 
 p=ggplot(rnafold_value_Amx, aes(x=as.numeric(V1)))+
   geom_histogram(binwidth = 0.5,color='black')+
   theme(axis.text = element_text(size=12,face = "bold",colour = "black", family = 'arial'),
         text = element_text(size=12,face = "bold",colour = "black", family = 'arial'))+
-  xlab('rnafold')
+  xlab('rnafold') +
+  ggtitle("RNAfold MFE values for the axolotl retrozyme Rtz331_353_Ax")
 
-ggsave('Rtz331_353_Ax_rnafold.png',p, width = 6, height = 6)
+Rtz331_353_Ax_rnafold_image = here("img","Rtz331_353_Ax_rnafold.png")
+ggsave(Rtz331_353_Ax_rnafold_image, p, width = 7, height = 6)
